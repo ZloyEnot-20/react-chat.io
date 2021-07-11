abbe6e622d78294e9dd8461846da270a77afba1f
ZloyEnot-20 <65668721+ZloyEnot-20@users.noreply.github.com>
2021-07-11 20:28:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       payload: obj,
     });
     socket.emit('ROOM:JOIN', obj);
+    console.log(obj, 'obj B INO')
     const { data } = await axios.get(`https://server-io.herokuapp.com/rooms/${obj.roomId}`); 
       dispatch({
       type: 'SET_DATA',
